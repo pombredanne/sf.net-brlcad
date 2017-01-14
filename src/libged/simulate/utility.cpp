7d10b87c3ebed01c5fa4c31b279faa19c1518f6d
ejno <ejno@2f96ce8b-6d43-0410-b8df-bffccc660ffb>
2017-01-14 21:58:23 +0000

    revise handling of member transformations for motion states; add a btIDebugDraw interface for output to brlcad databases; dynamically select ray grid tolerances (Bullet distance tolerances and physics step parameters should also be tuned); update the database at each btMotionState change, and create an rt_i at each iteration for each pair of primitives used in the collision algorithm

--- src/libged/simulate/utility.cpp
+++ src/libged/simulate/utility.cpp
@@ -1,4 +1,4 @@
-/*                  S I M U L A T I O N . H P P
+/*                     U T I L I T Y . C P P
  * BRL-CAD
  *
  * Copyright (c) 2014-2017 United States Government as represented by
@@ -17,51 +17,24 @@
  * License along with this file; see the file named COPYING for more
  * information.
  */
-/** @file simulation.hpp
+/** @file utility.cpp
  *
- * Programmatic interface for simulate.
+ * Helpers used by simulate.
  *
  */
 
 
-#ifndef SIMULATE_SIMULATION_H
-#define SIMULATE_SIMULATION_H
-
-
 #include "common.h"
 
-#include "rt_debug_draw.hpp"
-#include "rt_instance.hpp"
 
-#include "rt/rt_instance.h"
+#ifdef HAVE_BULLET
 
-#include <btBulletDynamicsCommon.h>
+
+#include "utility.hpp"
 
 
 namespace simulate
 {
-
-
-class Simulation
-{
-public:
-    explicit Simulation(db_i &db, const std::string &path);
-
-    void step(fastf_t seconds);
-
-
-private:
-    class Region;
-
-    RtDebugDraw m_debug_draw;
-    btDbvtBroadphase m_broadphase;
-    btDefaultCollisionConfiguration m_collision_config;
-    btCollisionDispatcher m_collision_dispatcher;
-    btSequentialImpulseConstraintSolver m_constraint_solver;
-    btDiscreteDynamicsWorld m_world;
-    std::vector<const Region *> m_regions;
-    const RtInstance m_rt_instance;
-};
 
 
 }
